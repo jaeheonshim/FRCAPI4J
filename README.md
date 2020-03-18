140a0c32a50caf75bba8fb43ad3bf9dfa76c0d26
Jaeheon Shim <git@jaeheonshim.com>
2020-03-18 12:54:11 -0400

    Finalize configurations and files for deployment

--- README.md
+++ README.md
@@ -50,5 +50,3 @@ for(Team team : teams) {
 
 ## Contributing
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
-
-Please make sure to update tests as appropriate.
--- src/main/java/FRCAPI.java
+++ src/main/java/FRCAPI.java
@@ -1,10 +1,10 @@
-import com.sun.istack.internal.NotNull;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import structures.*;
